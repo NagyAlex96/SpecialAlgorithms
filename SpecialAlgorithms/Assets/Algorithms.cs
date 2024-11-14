1f78cd8e6c4ffb3b477c4d10caba5f45e7a09216
NagyAlex96 <Nagy.Alex96@gmail.com>
2024-11-14 13:12:48 +0100

    this

--- SpecialAlgorithms/Assets/Algorithms.cs
+++ SpecialAlgorithms/Assets/Algorithms.cs
@@ -8,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace SpecialAlgorithms.Assets
 {
-    public partial class Algorithms
+    //TODO: refaktorálás
+    //partial class ha kell
+    //paraméterek nevei kisbetűsök
+    //nevek, változók, függvények illetve minden egyébnek ellenőrizni kell, hogy jól van-e írva a neve (kis és nagybetű)
+    //kommentezés
+    //kivételek?
+
+    //TODO: fennmaradó algoritmusok:
+    // Optimális megoldást visszaadó visszalépéses keresés
+
+
+    public static partial class Algorithms
     {
         /// <summary>
         /// Determine whether a collection has at least one element with a give condition
@@ -16,7 +27,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="condition">Conditional that returns true or false each value of T type</param>
         /// <returns><c>True</c> if at least 1 element meets the condition</returns>
-        public static bool Decision<T>(IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
+        public static bool Decision<T>(this IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
         {
             foreach (var item in collection)
             {
@@ -34,7 +45,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="seachedItem">Searched element</param>
         /// <returns><c>True</c> if the collection has at least one searched element</returns>
-        public static bool Decision<T>(IEnumerable<T> collection, T seachedItem) where T : IComparable<T>
+        public static bool Decision<T>(this IEnumerable<T> collection, T seachedItem) where T : IComparable<T>
         {
             foreach (var item in collection)
             {
@@ -52,7 +63,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="condition">Conditional that returns true or false each value of T type</param>
         /// <returns><c>True</c> + <c>Index</c> if at least one element exists in the collection with the given condition</returns>
-        public static (bool, int) LinearSearch<T>(IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
+        public static (bool, int) LinearSearch<T>(this IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
         {
             int index = 0;
             foreach (var item in collection)
@@ -70,14 +81,14 @@ namespace SpecialAlgorithms.Assets
         /// Determine whether a collection has at least one element with a give condition
         /// </summary>
         /// <param name="collection">Examined collection</param>
-        /// <param name="SeachedItem">Searched element</param>
+        /// <param name="seachedItem">Searched element</param>
         /// <returns><c>True</c> + <c>Index</c> if at least one element exists in the collection with the given condition</returns>
-        public static (bool, int) LinearSearch<T>(IEnumerable<T> collection, T SeachedItem) where T : IComparable<T>
+        public static (bool, int) LinearSearch<T>(this IEnumerable<T> collection, T seachedItem) where T : IComparable<T>
         {
             int index = 0;
             foreach (var item in collection)
             {
-                if (item.CompareTo(SeachedItem) == 0)
+                if (item.CompareTo(seachedItem) == 0)
                 {
                     return (true, index);
                 }
@@ -92,7 +103,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="condition">Conditional that returns true or false each value of T type</param>
         /// <returns><c>0</c> if there is no element with the given condition</returns>
-        public static int ElementCounter<T>(IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
+        public static int ElementCounter<T>(this IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
         {
             int counter = 0;
             foreach (var item in collection)
@@ -111,7 +122,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="seachedItem">Searched element</param>
         /// <returns><c>0</c> if there is no searched element</returns>
-        public static int ElementCounter<T>(IEnumerable<T> collection, T seachedItem) where T : IComparable<T>
+        public static int ElementCounter<T>(this IEnumerable<T> collection, T seachedItem) where T : IComparable<T>
         {
             int counter = 0;
             foreach (var item in collection)
@@ -127,15 +138,15 @@ namespace SpecialAlgorithms.Assets
         /// <summary>
         /// Search for the largest element of the collection
         /// </summary>
-        /// <param name="Collection">Examined collection</param>
+        /// <param name="collection">Examined collection</param>
         /// <returns>The largest element position <c>index</c> and <c>value</c></returns>
-        public static (int, T) MaxSearch<T>(IEnumerable<T> Collection) where T : IComparable<T>
+        public static (int, T) MaxSearch<T>(this IEnumerable<T> collection) where T : IComparable<T>
         {
             int maxIndex = -1;
             int index = -1;
-            T maxValue = Collection.ElementAt(0);
+            T maxValue = collection.ElementAt(0);
 
-            foreach (var item in Collection)
+            foreach (var item in collection)
             {
                 index++;
 
@@ -154,7 +165,7 @@ namespace SpecialAlgorithms.Assets
         /// <param name="collection">Examined collection</param>
         /// <param name="condition">Conditional that returns true or false each value of T type</param>
         /// <returns><c>Index</c> and  <c>Value</c> of the largest element.<c>-1</c> if there is no element with the given condition</returns>
-        public static (int, T) MaxSearch<T>(IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
+        public static (int, T) MaxSearch<T>(this IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
         {
             int maxIndex = -1;
             int index = -1;
@@ -176,15 +187,15 @@ namespace SpecialAlgorithms.Assets
         /// <summary>
         /// Search for the smallest element of the collection
         /// </summary>
-        /// <param name="Collection">Examined collection</param>
+        /// <param name="collection">Examined collection</param>
         /// <returns>The smallest element position <c>index</c> and <c>value</c></returns>
-        public static (int, T) MinSearch<T>(IEnumerable<T> Collection) where T : IComparable<T>
+        public static (int, T) MinSearch<T>(this IEnumerable<T> collection) where T : IComparable<T>
         {
             int minIndex = -1;
             int index = -1;
-            T minValue = Collection.ElementAt(0);
+            T minValue = collection.ElementAt(0);
 
-            foreach (var item in Collection)
+            foreach (var item in collection)
             {
                 index++;
 
@@ -200,16 +211,16 @@ namespace SpecialAlgorithms.Assets
         /// <summary>
         /// If the collection has at least one element with the given condition, search for the smallest element
         /// </summary>
-        /// <param name="Collection">Examined collection</param>
+        /// <param name="collection">Examined collection</param>
         /// <param name="condition">Conditional that returns true or false each value of T type</param>
         /// <returns><c>Index</c> and  <c>Value</c> of the largest element. <c>-1</c> if there is no element with the given condition</returns>
-        public static (int, T) MinSearch<T>(IEnumerable<T> Collection, Func<T, bool> condition) where T : IComparable<T>
+        public static (int, T) MinSearch<T>(this IEnumerable<T> collection, Func<T, bool> condition) where T : IComparable<T>
         {
             int minIndex = -1;
             int index = -1;
-            T minValue = Collection.ElementAt(0);
+            T minValue = collection.ElementAt(0);
 
-            foreach (var item in Collection)
+            foreach (var item in collection)
             {
                 index++;
 
@@ -222,19 +233,19 @@ namespace SpecialAlgorithms.Assets
             return condition(minValue) ? (minIndex, minValue) : (-1, minValue);
         }
 
-        public static object BackTrackSearch<T>(IEnumerable<IEnumerable<T>> Collection, out bool hasResult, bool needAllResult=false) where T : IComparable<T>
+        public static object BackTrackSearch<T>(this IEnumerable<IEnumerable<T>> collection, out bool hasResult, bool needAllResult=false) where T : IComparable<T>
         {
             IEnumerable<T> resultsCollection = CreateInstance<T>();
-            IEnumerable<IEnumerable<T>> allResultsCollection = CreateInstance<IEnumerable<T>>();
             hasResult = false;
 
             if (needAllResult)
             {
-                BackTrackSearchHelper(Collection, ref resultsCollection, ref allResultsCollection, ref hasResult);
+                IEnumerable<IEnumerable<T>> allResultsCollection = CreateInstance<IEnumerable<T>>();
+                BackTrackSearchHelper(collection, ref resultsCollection, ref allResultsCollection, ref hasResult);
                 return allResultsCollection;
             }
 
-            BackTrackSearchHelper(Collection, ref resultsCollection, ref hasResult);
+            BackTrackSearchHelper(collection, ref resultsCollection, ref hasResult);
 
             return resultsCollection;
         }
