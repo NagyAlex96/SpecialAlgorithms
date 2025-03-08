5c2d2b1f97d92a0530d899ef417553be7089144b
NagyAlex96 <Nagy.Alex96@gmail.com>
2025-03-08 10:04:26 +0100

    DevideGridMapToClusters done on both axis. Still testing

--- SpecialAlgorithms/Pathfinding/2D/Cluster.cs
+++ SpecialAlgorithms/Pathfinding/2D/Cluster.cs
@@ -1,8 +1,10 @@
 ﻿using SpecialAlgorithms.Pathfinding._2D.Interfaces;
+using System;
 
 namespace SpecialAlgorithms.Pathfinding._2D
 {
     public struct Cluster
+    {
     {
         const int CLUSTER_SIZE = 8;
         private GridMap[,] _gridClusters;
@@ -10,12 +12,42 @@ namespace SpecialAlgorithms.Pathfinding._2D
         public Cluster(in GridMap gridMap)
         {
             this._gridClusters = new GridMap[SetArraySize(gridMap.YLength), SetArraySize(gridMap.XLength)];
-            DivideGridMapToClusters(gridMap);
+            //DivideGridMapToClusters(gridMap);
         }
 
-        private void DivideGridMapToClusters(in GridMap gridMap)
+        public void DivideGridMapToClusters2(in GridMap gridMap)
         {
-            int yCoordinate = 0, xCoordinate = 0; //Iterators just like i, j in for-loop
+            int gridX = 0, gridY = 0;
+            int clusterX = 0, clusterY = 0;
+
+            while (gridY < gridMap.YLength)
+            {
+                while (gridX < gridMap.XLength)
+                {
+                    var clusterGrid = new GridMap(
+                        SetClusterSize(gridX, gridMap.XLength),
+                        SetClusterSize(gridY, gridMap.YLength)
+                    );
+
+                    for (int y = 0; y < clusterGrid.YLength; y++)
+                        for (int x = 0; x < clusterGrid.XLength; x++)
+                            clusterGrid.SetGridCoordinate(x, y, gridMap.GetGridCoordinate(gridX + x, gridY + y));
+
+                    AddClusterToGrid(clusterX, clusterY, clusterGrid);
+                    gridX += clusterGrid.XLength;
+                    clusterX++;
+                }
+
+                gridX = 0;
+                gridY += SetClusterSize(gridY, gridMap.YLength);
+                clusterX = 0;
+                clusterY++;
+            }
+        }
+
+        public void DivideGridMapToClusters(in GridMap gridMap)
+        {
+            int yCoordinate = 0, xCoordinate = 0;
             int clusterXPos = 0, clusterYPos = 0; //where to insert the current cluster 
             int gridCoordinateX = 0, gridCoordinateY = 0; //Iteratos like the recently one, but it help if there is more than 1 cluster
             GridMap clusterGrid = new GridMap(SetClusterSize(xCoordinate, gridMap.XLength), SetClusterSize(yCoordinate, gridMap.YLength));
@@ -35,14 +67,38 @@ namespace SpecialAlgorithms.Pathfinding._2D
                 }
                 else if (xCoordinate % clusterGrid.XLength == 0 && yCoordinate % clusterGrid.YLength == 0)
                 {
-                    AddClusterToGrid(clusterXPos++, clusterYPos, clusterGrid);
-
-                    yCoordinate = 0;
+                    if (gridCoordinateX < gridMap.XLength && clusterXPos < _gridClusters.GetLength(1))
+                    {
+                        gridCoordinateX += clusterGrid.XLength;
+                        AddClusterToGrid(clusterXPos++, clusterYPos, clusterGrid);
+                    }
+                    else if (gridCoordinateY < gridMap.YLength && clusterYPos + 1 < _gridClusters.GetLength(0))
+                    {
+                        gridCoordinateY += clusterGrid.YLength;
+                        AddClusterToGrid(clusterXPos, clusterYPos++, clusterGrid);
+                    }
+                    else
+                    {
+                        AddClusterToGrid(clusterXPos, clusterYPos, clusterGrid);
+                        break;
+                    }
                     xCoordinate = 0;
-                    gridCoordinateX += xCoordinate + clusterGrid.XLength;
+                    yCoordinate = 0;
+
                     if (gridCoordinateX < gridMap.XLength)
                     {
                         clusterGrid = new GridMap(SetClusterSize(gridCoordinateX, gridMap.XLength), SetClusterSize(gridCoordinateY, gridMap.YLength));
+                    }
+                    else if (gridCoordinateY < gridMap.YLength)
+                    {
+                        gridCoordinateY += clusterGrid.YLength;
+                        if (gridCoordinateX == gridMap.XLength && gridCoordinateY != gridMap.YLength)
+                        {
+                            gridCoordinateX = 0;
+                            clusterXPos = 0;
+                            clusterYPos++;
+                            clusterGrid = new GridMap(SetClusterSize(gridCoordinateX, gridMap.XLength), SetClusterSize(gridCoordinateY, gridMap.YLength));
+                        }
                     }
                 }
             }
@@ -101,13 +157,17 @@ namespace SpecialAlgorithms.Pathfinding._2D
 
         public bool AllTrue()
         {
-            bool allTrue = false;
-            foreach (var x in _gridClusters)
-            {
-                allTrue = x.AllTrue();
-            }
+            bool ok = false;
 
-            return allTrue;
+
+            for (int y = 0; y < this._gridClusters.GetLength(0); y++)
+            {
+                for (int x = 0; x < this._gridClusters.GetLength(1); x++)
+                {
+                    ok = this._gridClusters[y, x].AllTrue();
+                }
+            }
+            return ok;
         }
     }
 }
