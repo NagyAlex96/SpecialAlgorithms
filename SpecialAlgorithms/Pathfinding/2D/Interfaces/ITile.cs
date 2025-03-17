23566fbecfba030ce0bef54ab4ee03e77d15fee0
NagyAlex96 <Nagy.Alex96@gmail.com>
2025-03-17 16:20:29 +0100

    ITile & Tile refactored

--- SpecialAlgorithms/Pathfinding/2D/Interfaces/ITile.cs
+++ SpecialAlgorithms/Pathfinding/2D/Interfaces/ITile.cs
@@ -1,10 +1,34 @@
 ﻿namespace SpecialAlgorithms.Pathfinding._2D.Interfaces
 {
-    public interface ITile
+    public interface ITile : IComparable<ITile>
     {
-        public int XPos { get; }
-        public int YPos { get; }
+        /// <summary>
+        /// X-Coordinate
+        /// </summary>
+        public short XPos { get; }
+
+        /// <summary>
+        /// Y-Coordinate
+        /// </summary>
+        public short YPos { get; }
+
         public bool IsConnectionPoint { get; }
+
         public byte TerrainType { get; }
+
+        /// <summary>
+        /// Heuristic Cost
+        /// </summary>
+        public int HCost { get; set; }
+
+        /// <summary>
+        /// Walking Cost
+        /// </summary>
+        public int GCost { get; set; }
+
+        /// <summary>
+        /// Summa of Heuristic and Walking Cost
+        /// </summary>
+        public int FCost { get; }
     }
 }
--- SpecialAlgorithms/Pathfinding/2D/Tile.cs
+++ SpecialAlgorithms/Pathfinding/2D/Tile.cs
@@ -1,26 +1,49 @@
-﻿using SpecialAlgorithms.Pathfinding._2D.Interfaces;
+﻿using SpecialAlgorithms.Assets;
+using SpecialAlgorithms.Pathfinding._2D.Interfaces;
+
 
 namespace SpecialAlgorithms.Pathfinding._2D
 {
     public struct Tile : ITile
     {
-        [Obsolete("You must not use this!",true)]
+        [Obsolete("You must not use this!", true)]
         public Tile()
         {
-            
+
         }
 
         public Tile(int xPos, int yPos, TerrainType terrainType)
         {
-            this.XPos = xPos;
-            this.YPos = yPos;
+            ExceptionHelper.ThrowIfArgumentOutOfRange(xPos, short.MinValue + 1, short.MaxValue);
+            ExceptionHelper.ThrowIfArgumentOutOfRange(yPos, short.MinValue + 1, short.MaxValue);
+
+            this.XPos = (short)xPos;
+            this.YPos = (short)yPos;
             this.TerrainType = (byte)terrainType;
         }
 
-        public int XPos { get; set; }
-        public int YPos { get; set; }
+        public short XPos { get; }
+        public short YPos { get; }
+        public byte TerrainType { get; }
+        public bool IsConnectionPoint { get; }
 
-        public byte TerrainType { get; set; }
-        public bool IsConnectionPoint { get; set; }
+        public int HCost { get; set; }
+        public int GCost { get; set; }
+        public int FCost => GCost + HCost;
+
+        public int CompareTo(ITile? other)
+        {
+            ExceptionHelper.ThrowIfArgumentIsNull(other, "Null value cannot be compared to something!");
+
+            int comparer = other.FCost.CompareTo(this.FCost);
+            if (comparer != 0)
+                return comparer;
+
+            comparer = other.GCost.CompareTo(this.GCost);
+            if (comparer != 0)
+                return comparer;
+
+            return other.HCost.CompareTo(this.HCost);
+        }
     }
 }
