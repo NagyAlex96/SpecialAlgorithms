b5383357385a1de0804cadce7860712c67ec6d6e
NagyAlex96 <Nagy.Alex96@gmail.com>
2025-03-17 16:21:29 +0100

    ICluster & Cluster Refactored

--- SpecialAlgorithms/Pathfinding/2D/Cluster.cs
+++ SpecialAlgorithms/Pathfinding/2D/Cluster.cs
@@ -1,18 +1,27 @@
-﻿using SpecialAlgorithms.Pathfinding._2D.Interfaces;
+﻿using SpecialAlgorithms.Assets;
+using SpecialAlgorithms.Pathfinding._2D.Interfaces;
 
 namespace SpecialAlgorithms.Pathfinding._2D
 {
     public struct Cluster : ICluster
     {
-        public ITile[,] ClusterCollection { get; }
+        [Obsolete("You must not use this!", true)]
+        public Cluster()
+        {
 
-        public int XSize => this.ClusterCollection.GetLength(1);
-
-        public int YSize => this.ClusterCollection.GetLength(0);
+        }
 
         public Cluster(int xSize, int ySize)
         {
+            ExceptionHelper.ThrowIfArgumentOutOfRange(xSize, 0, Condition.LessOrEqualThan);
+            ExceptionHelper.ThrowIfArgumentOutOfRange(ySize, 0, Condition.LessOrEqualThan);
+
             this.ClusterCollection = new ITile[ySize, xSize];
         }
+
+        public ITile[,] ClusterCollection { get; }
+        public List<IPathNode> PathNodes { get; }
+        public int XSize => this.ClusterCollection.GetLength(1);
+        public int YSize => this.ClusterCollection.GetLength(0);
     }
 }
--- SpecialAlgorithms/Pathfinding/2D/Interfaces/ICluster.cs
+++ SpecialAlgorithms/Pathfinding/2D/Interfaces/ICluster.cs
@@ -4,7 +4,19 @@
     {
         public ITile[,] ClusterCollection { get; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<IPathNode> PathNodes { get; }
+
+        /// <summary>
+        /// Horizontal size
+        /// </summary>
         public int XSize { get; }
+
+        /// <summary>
+        /// Vertical size
+        /// </summary>
         public int YSize { get; }
     }
 }
