f99884c32e8c2e1093817794f6d2ecc78fbfcc6c
NagyAlex96 <Nagy.Alex96@gmail.com>
2025-03-17 08:34:09 +0100

    Tile Refactor

--- SpecialAlgorithms/Pathfinding/2D/Interfaces/ITile.cs
+++ SpecialAlgorithms/Pathfinding/2D/Interfaces/ITile.cs
@@ -2,9 +2,9 @@
 {
     public interface ITile
     {
-        public int XPos { get; set; }
-        public int YPos { get; set; }
-        public bool IsConnectionPoint {  get; set; }
-        public TerrainType TerrainType { get; set; }
+        public int XPos { get; }
+        public int YPos { get; }
+        public bool IsConnectionPoint { get; }
+        public byte TerrainType { get; }
     }
 }
--- SpecialAlgorithms/Pathfinding/2D/Tile.cs
+++ SpecialAlgorithms/Pathfinding/2D/Tile.cs
@@ -4,17 +4,23 @@ namespace SpecialAlgorithms.Pathfinding._2D
 {
     public struct Tile : ITile
     {
+        [Obsolete("You must not use this!",true)]
+        public Tile()
+        {
+            
+        }
+
         public Tile(int xPos, int yPos, TerrainType terrainType)
         {
             this.XPos = xPos;
             this.YPos = yPos;
-            this.TerrainType = terrainType;
+            this.TerrainType = (byte)terrainType;
         }
 
         public int XPos { get; set; }
         public int YPos { get; set; }
 
-        public TerrainType TerrainType { get; set; }
+        public byte TerrainType { get; set; }
         public bool IsConnectionPoint { get; set; }
     }
 }
